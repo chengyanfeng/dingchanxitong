f62f3447aae8208c3b236208776d309329b71670
yanfeng <chengyanfengchengyanfeng@genyuanlian.com>
2018-11-30 12:02:41 +0800

    ok

--- models/db.go
+++ models/db.go
@@ -5,7 +5,7 @@ type P map[string]interface{}
 var Mysqlconn = P{
 
 	"username": "root",
-	"password": "root",
+	"password": "1234",
 	"host":     "localhost",
 	"port":     3306,
 	"name":     "test",
